8000fa6fbf39204e1e842abca6c1437cd89f0cb4
Sandro Boschetti <sandro@gmail.com>
2022-12-13 13:41:11 -0300

    Tentativa 2

--- src/main/java/srb/model/TradePerformanceModel.java
+++ src/main/java/srb/model/TradePerformanceModel.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-//@Entity
+@Entity
 public class TradePerformanceModel implements Serializable{
 
 	private static final long serialVersionUID = 1L;
