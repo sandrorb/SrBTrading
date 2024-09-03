9b7ea6dbb7b8108a8f4782c417875521b42b2a97
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2024-09-03 07:35:56 -0300

    nada demais

--- src/main/java/srb/SrBTradingMain.java
+++ src/main/java/srb/SrBTradingMain.java
@@ -2,6 +2,7 @@ package srb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.ApiConstants;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -11,6 +12,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import srb.telegram.SrBTelegram;
 import srb.telegram.bot.SrBTelegramBot;
 import srb.telegram.bot.SrBTelegramBotMain;
+
+//extends SpringBootServletInitializer dever ser usado se for fazer deploy como war file.
 
 @SpringBootApplication
 public class SrBTradingMain {
